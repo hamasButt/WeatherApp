577a61b58159ca8dc96fdaf37809ec1d2110e748
Hamas Ahmed <89587893+hamasButt@users.noreply.github.com>
2022-05-17 17:15:24 +0500

    Update WeatherApp.js

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -11,7 +11,7 @@ export const WeatherApp = () => {
         search('Rawalpindi')
         setQuery('')
 
-    }, [])
+    }, [search,setQuery])
 
     const searchCity = (e) => {
         if (e.key === 'Enter') {
