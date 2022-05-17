652378d4db4c31a52fb9a7585f92e8e03547853b
Hamas Ahmed <hamasahmed341@gmail.com>
2022-05-17 16:55:37 +0500

    Added Context Api

---
+++ src/services/context/app-state.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AppContext from "./AppContext";
+import {useState} from "react";
+import axios from "axios";
+import api from "../../assets/weather_api/api";
+
+export const AppState = ({children}) => {
+    const [weathers, setWeathers] = useState('')
+    const [query, setQuery] = useState('')
+
+    const search = (cityName) => {
+        console.log(cityName)
+        axios.get(`${api.baseUrl}weather?q=${query === '' ? cityName : query}&units=metric&APPID=${api.key}`)
+            .then(result => {
+                setWeathers(result.data)
+                setQuery('')
+                console.log(result.data)
+            })
+    }
+
+    const store = {
+        weathers,
+        setWeathers,
+        setQuery,
+        query,
+        search
+    }
+    return (
+        <AppContext.Provider value={store}>
+            {children}
+        </AppContext.Provider>
+    )
+}
